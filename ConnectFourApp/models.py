3c6555f2e6e5188d1e621b3f5ed0ed90014c1462
Grant <gjamespowell@gmail.com>
2017-02-12 09:14:07 -0500

    added the basic tests for the gameboard class along with the first part of the find_winner method

--- ConnectFourApp/models.py
+++ ConnectFourApp/models.py
@@ -5,6 +5,7 @@ import json
 # Create your models here.
 width = 7
 depth = 6
+needed_to_win = 4
 empty_board = [[0] * width for _ in range(depth)]
 
 # empty_board
@@ -20,6 +21,8 @@ empty_board = [[0] * width for _ in range(depth)]
 
 class GameBoard(models.Model):
     game_data = models.CharField(max_length=140, default=json.dumps(empty_board))
+    # if you increase the game board, you would need a larger max_length of game_data
+    winner = models.IntegerField(default=0)
 
     def get_game_board(self):
         return json.loads(self.game_data)
@@ -32,16 +35,64 @@ class GameBoard(models.Model):
         if board[0][slot]:  # This means that that slot was full, so this is an error
             return 1  # Error
         for i in reversed(range(depth)):
-            if not board[i][slot]:
-                board[i][slot] = player
+            if not board[i][slot]:  # this means that that slot has a 0 in it (i.e. is unused)
+                board[i][slot] = player  # mark the lowest open row of the column with the player number
                 break
-        self.__set_game_board(board)
+        self.__set_game_board(board)  # set the game board property of the object
+        self.winner = self.find_winner(board)  # set the winner if there is one
 
-    def has_winner(self):
-        return False
+    @property
+    def stalemate(self):
+        board = self.get_game_board()
+        no_winner = self.winner == 0
+        no_moves_remaining = all(board[0])
+        return no_winner and no_moves_remaining
+
+    @staticmethod
+    def find_winner(board):
+
+        # check columns for winner
+        for col in range(width):  # iterate through the columns
+            # iterate through the starting depths, if there is a max depth of 6 that means we only have to check
+            # starting at depth 0, 1, 2 because at depth 3 there wouldn't be enough to have a connect 4
+            for pos in range(depth + 1 - needed_to_win):  # you need plus one because range is weird
+                # then we check if the first position is a zero, if it is that means that it can't be a connect four
+                # beacuse there is an empty spot, this is to take care of the case where all 4 of positions
+                # we are checking are the same but empty
+                if board[pos][col] != 0:
+                    # this looks a little tricky but isn't. the hash set adds values to itself
+                    # for every unique value, if at the end there was only one unique value, that means that all the
+                    # elements were the same value, which would signify that there was a winner
+                    uniquevals = set(board[i][col] for i in range(pos, pos + needed_to_win))
+                    if len(uniquevals) == 1:
+                        # return the unique element which is equal to the player number
+                        return board[pos][col]
+
+        # check the rows for a winner
+        for row in board:
+            # iterate through the row for every start position with enough length remaining to have a winner.
+            # For a 7 width game we would only need to look at positions 0,1,2,3 because position 4 would only
+            # have 3 slots left in the row, so it couldn't have a connect 4
+            for pos in range(width - needed_to_win):
+                # check to make sure that the first spot isn't empty, if they were all the same but empty, we wouldn't
+                # have a want to return 0 as the winner
+                if row[pos] != 0:
+                    # check to see that the number of unique entries is equal to one
+                    if len(set(row[pos: pos + needed_to_win])) == 1:
+                        # return the first entry, equal to the player number
+                        return row[pos]
+
+        # check right diagonals for a winner
+
+
+        # check left diagonals for a winner
+
+
+        # no winner was found
+        return 0
 
 
 class Game(models.Model):
     user = models.ForeignKey(User)
-    isusersturn = models.BooleanField(default=0)  #  0 if computer's turn 1 is user's turn
+    isusersturn = models.BooleanField(default=0)  # 0 if computer's turn 1 is user's turn
     gameboard = models.OneToOneField(GameBoard)
--- ConnectFourApp/tests.py
+++ ConnectFourApp/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
+from .models import GameBoard, Game
 
 # Create your tests here.
+
+
+class GameBoardTests(TestCase):
+
+    def test_invalid_move(self):
+        board = GameBoard()
+        moves = (0,0,0,0,0,0)  # Fill a Column
+        player = 1
+        for move in moves:
+            error = board.make_move(player, move)
+            self.assertEqual(error, None)  # assert there is no error
+        move = 0  # add one to a filled row
+        error = board.make_move(player, move)
+        self.assertEqual(error, 1)
+
+    def test_stalemate(self):
+        board = GameBoard()
+        evens = (0,2,4,6)
+        odds = (1,3,5)
+        for _ in range(3):
+            for move in evens:
+                board.make_move(1, move)
+            for move in odds:
+                board.make_move(2, move)
+        for _ in range(3):
+            for move in odds:
+                board.make_move(1, move)
+            for move in evens:
+                board.make_move(2, move)
+        self.assertTrue(board.stalemate)
+
+    def test_horizontal_victory(self):
+        board = GameBoard()
+        player = 1
+        moves = (0,1,2,3)
+        for move in moves:
+            board.make_move(player, move)
+        self.assertEqual(player, board.winner)
+
+    def test_veritical_victory(self):
+        board = GameBoard()
+        player = 1
+        moves = (0,0,0,0)
+        for move in moves:
+            board.make_move(player, move)
+        self.assertEqual(player, board.winner)
+
+    def test_right_diagonal_win(self):
+        board = GameBoard()
+        p2moves = (0,0,0,1,1,2)  # make a little triangle to put the winning diagonal on
+        p1moves = (0,1,2,3)
+        for move in p2moves:
+            board.make_move(2, move)
+        for move in p1moves:
+            board.make_move(1, move)
+        self.assertEqual(board.winner, 1)
+
+    def test_left_diagonal_win(self):
+        board = GameBoard()
+        p2moves = (3,3,3,2,2,1)
+        p1moves = (3,2,1,0)
+        for move in p2moves:
+            board.make_move(2, move)
+        for move in p1moves:
+            board.make_move(1, move)
+        self.assertEqual(board.winner, 1)
+
+
+
