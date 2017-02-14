861fb1992620db34db77b2bd9fc87ce1198c8360
Grant <gjamespowell@gmail.com>
2017-02-14 10:34:07 -0500

    made secret key dev loading

--- ConnectFourProject/settings.py
+++ ConnectFourProject/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .secrets import SECRET_KEY  # This requires the secrets.py file to be in the same directory,
+try:
+    from .secrets import SECRET_KEY # This requires the secrets.py file to be in the same directory,
+except (ImportError, SystemError):
+    SECRET_KEY = "not so secret key"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
